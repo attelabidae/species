0d8c2e934034cfcc09c3bc235879008a7716d64f
Samanta Orellana <kasaore@gmail.com>
2025-08-19 13:04:28 -0500

    Update SectionDescription.vue

--- pages/components/Section/SectionDescription.vue
+++ pages/components/Section/SectionDescription.vue
@@ -4,14 +4,14 @@
       <div class="prose !container dark:prose-invert">
         <div>
           <p>
-            Welcome to <strong class="font-semibold"> Anthribidae of the World Online</strong>, a virtual catalog that aims to facilitate the search of fungus weevil taxa. The taxonomic database is actively fed on <a 
+            Welcome to <strong class="font-semibold"> Attelabidae of the World Online</strong>, a virtual catalog that aims to facilitate the search leaf-rolling weevils taxa. The taxonomic database is actively fed on <a 
       href="https://taxonworks.org" 
       class="no-underline text-gray-700 font-medium hover:text-gray-900 hover:underline focus:outline-none focus:ring-2 focus:ring-gray-400 transition"
       target="_blank" 
       rel="noopener noreferrer"
     >
       TaxonWorks
-    </a> following specialized scientific literature. Specimen records and images are the product of the curation, identification, imaging and digitization of Anthribidae shared by several collections.   
+    </a> following specialized scientific literature. Specimen records and images are the product of the curation, identification, imaging and digitization of Attelabidae in the ASU O'Brien Collection.   
           </p>
          </div>
       </div>
